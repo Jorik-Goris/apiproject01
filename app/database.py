1b0d638d3c25cd3b906683d8e6e157efc579892a
Jorik-Goris <95848835+Jorik-Goris@users.noreply.github.com>
2023-11-12 17:04:58 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://root:abc123!@localhost:3306/mydb"
+SQLALCHEMY_DATABASE_URL = "mysql://jorik:abc123!@mysql:3306/mydb"
 
 # Create the SQLAlchemy engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
